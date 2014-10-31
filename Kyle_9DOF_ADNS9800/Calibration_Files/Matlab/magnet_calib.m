2f635890b996a35feb7b5d24c912b242f26e3d99
kbcutler58 <kbcutler58@gmail.com>
2014-10-31 12:18:28 -0700

    Edited magnet calibration file and instructions

--- Kyle_9DOF_ADNS9800/Calibration_Files/Matlab/magnet_calib.m
+++ Kyle_9DOF_ADNS9800/Calibration_Files/Matlab/magnet_calib.m
@@ -6,8 +6,8 @@
 
 clear 
 clc
-cd 'C:\Users\BLI\Desktop\Downloads\ProbeTracker-master (7)\ProbeTracker-master\Kyle_9DOF_ADNS9800\Calibration_Files\Processing\magnet_text_gen'
-cal_data = load('Magnet_Cal_Data6.txt');
+cd 'C:\Users\Kyle\Documents\GitHub\ProbeTracker\Kyle_9DOF_ADNS9800\Calibration_Files\Processing\magnet_text_gen'
+cal_data = load('Magnet_Cal_Data3.txt');
 x = cal_data(:,1);
 y = cal_data(:,2);
 z = cal_data(:,3);
@@ -16,7 +16,7 @@ scatter3(x,y,z)
 axis equal
 
 %%
-[a,b,c,d] = ellipsoid_fit(cal_data);
+[a,b,c] = ellipsoid_fit(cal_data);
 
 close all
 hold on
@@ -25,20 +25,26 @@ scatter3(x,y,z)
 surf(x1,y1,z1)
 view(3)
 %%
-S = [x - a(1), y - a(2), z - a(3)]';
+% Scaling Matrix
 scale = inv([b(1) 0 0; 0 b(2) 0; 0 0 b(3)]) * min(b);
 map = c';
 invmap = c;
 comp = invmap * scale * map;
+
+% Scaled Points
+S = [x - a(1), y - a(2), z - a(3)]';
 S = comp * S;
 
+fprintf(' To use extended calibration add the next two lines \n')
 fprintf('const float magn_ellipsoid_center[3] = {%.6g, %.6g, %.6g};\n', a);
 fprintf('const float magn_ellipsoid_transform[3][3] = {{%.6g, %.6g, %.6g}, {%.6g, %.6g, %.6g}, {%.6g, %.6g, %.6g}};\n', comp);
-
+fprintf('\n');
 %% For use in hard iron calibration
-maxMag(1) = max(x);
-maxMag(2) = max(y);
-maxMag(3) = max(z);
-minMag(1) = min(x);
-minMag(2) = min(y);
-minMag(3) = min(z);
+
+fprintf(' To use regular calibration add the next few lines: \n')
+fprintf('#define magnet_x_min ((float) %.4g) \n', min(x))
+fprintf('#define magnet_y_min ((float) %.4g) \n', min(y))
+fprintf('#define magnet_z_min ((float) %.4g) \n', min(z))
+fprintf('#define magnet_x_max ((float) %.4g) \n', max(x))
+fprintf('#define magnet_y_max ((float) %.4g) \n', max(y))
+fprintf('#define magnet_z_max ((float) %.4g) \n', max(z))
